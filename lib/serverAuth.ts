bc8858522cf0d313c548565a8dabc5333e9163e3
Yashwanth <84189123+Yash981@users.noreply.github.com>
2024-01-01 19:43:25 +0530

    Update serverAuth.ts

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -10,7 +10,7 @@ const serverAuth =async (req:NextApiRequest,res:NextApiResponse) => {
     if(!session?.user?.email){
         throw new Error('Not signed in')
     }
-    const currentUser = await prismadb.user.findUnique({ where:{email: session?.user?.email}})
+    const currentUser = await prismadb.user.findUnique({ where:{email: session.user.email}})
 
     if(!currentUser){
         throw new Error('Not signed in')
