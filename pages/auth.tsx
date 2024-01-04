47676b73d1b07b61b36d73cd540032d76adf50de
Yashwanth <84189123+Yash981@users.noreply.github.com>
2024-01-04 12:49:27 +0530

    Update auth.tsx

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,13 +3,32 @@
 import Input from "@/components/Input";
 import axios from "axios";
 import { useCallback, useState } from "react";
-import { getSession, signIn } from "next-auth/react";
+import {  signIn } from "next-auth/react";
 // import { useRouter } from "next/router";
-import { FcGoogle } from "react-icons/fc"
-import { FaGithub } from "react-icons/fa"
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { GetServerSidePropsContext } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
+import { useRouter } from "next/router";
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
 
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 const Auth = () => {
-  // const router = useRouter();
+  const router = useRouter();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -26,14 +45,14 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        redirect: true,
-        callbackUrl: "/profiles",
+        redirect: false,
+        callbackUrl: "/",
       });
-      // router.push("/");
+      router.push("/profiles");
     } catch (error) {
       console.log(error);
     }
-  }, [email, password]);
+  }, [email, password,router]);
 
   const register = useCallback(async () => {
     try {
@@ -44,7 +63,7 @@ const Auth = () => {
       });
       login();
     } catch (error) {
-      console.log(error);
+      console.log(error, "errrorrr");
     }
   }, [email, name, password, login]);
 
@@ -94,11 +113,17 @@ const Auth = () => {
               {variant === "login" ? "Login" : "Sign up"}
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-              <div onClick={()=> signIn('google',{callbackUrl:'/profiles'})}  className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-                <FcGoogle size={30}/>
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/profiles" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={30} />
               </div>
-              <div onClick={()=> signIn('github',{callbackUrl:'/profiles'})} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-                <FaGithub size={30}/>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/profiles" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={30} />
               </div>
             </div>
             <p className="text-neutral-500 mt-12">
